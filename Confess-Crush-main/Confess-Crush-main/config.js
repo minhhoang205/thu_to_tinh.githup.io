affe9b640d23acbcfd9adc490e12f252c7ab8642
minhhoang205 <106818439+minhhoang205@users.noreply.github.com>
2022-06-03 20:49:38 +0700

    Update config.js

--- Confess-Crush-main/Confess-Crush-main/config.js
+++ Confess-Crush-main/Confess-Crush-main/config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+    titleWeb: "with love",
+    introTitle: 'with love',
+    introDesc: `T muốn order một ly nước ép cam
+    tâm tình nguyện yêu em`,
+    btnIntro: 'come here bae',
+    title: 'Mình đã quá thích cậu rồi. Phải làm sao Phải làm sao🥰😵‍💫',
+    desc: 'Cậu thấy vậy có được hok 😵‍💫😵‍💫 ',
+    btnYes: 'cx có chút thích m <33',
+    btnNo: 'sao cx đc :3',
+    question: 'làm thao để trở thành bn trai bae đây 😶‍🌫️😶‍🌫️',
+    btnReply: 'send it here bae <3',
+    reply: 'Yêu m',
+    mess: 'SAU CÙNG THÌ',
+    messDesc: 'M đồng ý làm bạn gái t nhaaaa',
+    btnAccept: 't đồng ý 
+    (ko còn lựa chọn khác âu bae-ehe)',
+    messLink: 'https://github.com/minhhoang205/thư_tỏ_tình' //https://www.facebook.com/profile.php?id=100050931922709
